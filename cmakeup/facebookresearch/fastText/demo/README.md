7b68545dda73733af8b2c17ffc1df9960a77b2e1
innerNULL <lyt697@outlook.com>
2021-04-07 15:19:02 +0800

    Update demo.

--- cmakeup/facebookresearch/fastText/demo/README.md
+++ cmakeup/facebookresearch/fastText/demo/README.md
@@ -7,3 +7,6 @@ mkdir build && cd build
 cmake ../
 make -j4
 ```
+
+# Note
+* Have to open `pthread` compiling flag, such as `set(CMAKE_CXX_FLAGS " -pthread -std=c++17 -funroll-loops -O3 -march=native")`.
---
+++ cmakeup/facebookresearch/fastText/demo/demo.cpp
@@ -0,0 +1,63 @@
+/// file: demo.cpp
+/// date: 2021-03-12
+
+
+#include <cstdlib>
+#include <sstream>
+#include <utility>
+#include "fasttext.h"
+
+
+
+
+int32_t prediction_demo(const std::string& model_path, 
+        const std::string& model_input) {
+  printf("Starts prediction_demo\n");
+  system("pwd");
+  /// ERROR:
+  //std::shared_ptr<fasttext::FastText> model_ptr;
+  //model_ptr->loadModel(model_path);
+  
+  // ERROR:
+  //std::auto_ptr<fasttext::FastText> model_ptr;
+  //model_ptr->loadModel(model_path);
+
+  fasttext::FastText model;
+  model.loadModel(model_path);
+
+  std::stringstream model_input_;
+  model_input_ << model_input;
+  //std::istream& model_input__ = model_input_;
+
+  std::vector< std::pair<float, std::string> > predictions;
+  model.predictLine(model_input_, predictions, 3, 0.01);
+
+  for (std::vector< std::pair<float, std::string> >::iterator iter = predictions.begin(); 
+          iter < predictions.end(); ++iter) {
+    std::cout << "category: " << iter->second << ", score: " << iter->first << std::endl;
+  }
+
+  printf("Finished prediction_demo\n");
+  return 0;
+}
+
+
+
+
+int main() {
+  const std::string MODEL_URL = "https://dl.fbaipublicfiles.com/fasttext/supervised-models";
+  const std::string MODEL_NAME = "sogou_news.ftz";
+
+  std::string model_uri = MODEL_URL + "/" + MODEL_NAME;
+  std::string model_path = "./" + MODEL_NAME;
+
+  //std::vector<std::string> model_input = {
+  //    "上", "海", "老", "相", "机", "制", "造", "博", "物", "馆", "即", "将", "开", "门", "迎", "客"
+  //};
+  std::string model_input = "上 海 老 相 机 制 造 博 物 馆 即 将 开 门 迎 客";
+
+  system(("rm -rf " + model_path + " && wget " + model_uri).c_str());
+
+  prediction_demo(model_path, model_input); 
+  return 0;
+}
