e8d6e80c685e3eaad3d8c4a92c6a9a54a1fd76b5
innerNULL <lyt697@outlook.com>
2021-04-09 17:43:51 +0800

    [Feat]: Init adds librdkafka pkg.

---
+++ cmakeup/pkg/edenhill/librdkafka/demo/demo.cpp
@@ -0,0 +1,10 @@
+/// file: demo.cpp
+/// date: 2021-04-09
+
+
+#include <rdkafkacpp.h>
+
+
+int main() {
+  return 0;
+}
---
+++ cmakeup/pkg/edenhill/librdkafka/librdkafka_pkg.cmake
@@ -0,0 +1,48 @@
+# file: librdkafka_pkg.cmake
+# date: 2021-04-09
+
+
+cmake_minimum_required(VERSION 2.8.12)
+
+set(THIRDPARTY_ROOT "./_3rdparty")
+set(VERSION "master")
+set(ORGANIZATION "edenhill")
+set(RESPOSITORY "librdkafka")
+
+
+# https://github.com/edenhill/librdkafka/archive/refs/heads/master.zip 
+set(PROJ ${ORGANIZATION}/${RESPOSITORY})
+set(TARGET_URL "https://github.com/${PROJ}/archive/refs/heads/${VERSION}.zip")
+execute_process(COMMAND pwd OUTPUT_VARIABLE CURR_PATH)
+string(REPLACE "\n" "" CURR_PATH ${CURR_PATH})
+get_filename_component(CURR_PATH ${CURR_PATH} ABSOLUTE)
+
+set(SRC_FOLDER_NAME ${RESPOSITORY}-${VERSION})
+
+
+if(EXISTS "${THIRDPARTY_ROOT}/${SRC_FOLDER_NAME}")
+  message(STATUS "${THIRDPARTY_ROOT}/${SRC_FOLDER_NAME} exists")
+else()
+  execute_process(COMMAND wget ${TARGET_URL})
+  execute_process(COMMAND unzip ${VERSION}.zip)
+  execute_process(COMMAND mkdir -p ${THIRDPARTY_ROOT})
+  execute_process(COMMAND mv ${SRC_FOLDER_NAME} ${THIRDPARTY_ROOT})
+  execute_process(COMMAND mv ${VERSION}.zip ${THIRDPARTY_ROOT}/${SRC_FOLDER_NAME}.zip)
+  execute_process(COMMAND mkdir -p ${THIRDPARTY_ROOT}/${SRC_FOLDER_NAME}/build)
+  execute_process(COMMAND cmake ../ -DBUILD_GMOCK=OFF -DRDKAFKA_BUILD_STATIC=ON 
+      WORKING_DIRECTORY ${THIRDPARTY_ROOT}/${SRC_FOLDER_NAME}/build)
+  execute_process(COMMAND make -j8 WORKING_DIRECTORY ${THIRDPARTY_ROOT}/${SRC_FOLDER_NAME}/build)
+endif()
+
+
+set(${RESPOSITORY}_include_path
+  "${CURR_PATH}/${THIRDPARTY_ROOT}/${SRC_FOLDER_NAME}/src-cpp")
+set(${RESPOSITORY}_lib 
+  "${CURR_PATH}/${THIRDPARTY_ROOT}/${SRC_FOLDER_NAME}/build/src-cpp/librdkafka++.a"
+)
+
+message(STATUS "${RESPOSITORY}_include_path: ${${RESPOSITORY}_include_path}")
+message(STATUS "${RESPOSITORY}_lib: ${${RESPOSITORY}_lib}")
+
+include_directories(${${RESPOSITORY}_include_path})
+message(STATUS "You should add `target_link_libraries(\${__TARGET_BIN__} PUBLIC \${${RESPOSITORY}_lib})` for target-bin.")
