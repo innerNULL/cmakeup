c5df91baa32760473176b02224dce20e2a11bf12
innerNULL <lyt697@outlook.com>
2021-04-16 23:43:28 +0800

    Update demo.

--- cmakeup/pkg/oatpp/oatpp/demo/demo.cpp
+++ cmakeup/pkg/oatpp/oatpp/demo/demo.cpp
@@ -2,10 +2,19 @@
 /// date: 2021-04-16
 
 
+#include "oatpp/web/client/HttpRequestExecutor.hpp"
 #include "oatpp/network/tcp/client/ConnectionProvider.hpp"
-#include "oatpp/core/async/Executor.hpp"
+
+#include "oatpp/parser/json/mapping/ObjectMapper.hpp"
 
 
 int main(int argc, char **argv) {
+  oatpp::base::Environment::init();
+
+  auto objectMapper = oatpp::parser::json::mapping::ObjectMapper::createShared();
+  auto connectionProvider = oatpp::network::tcp::client::ConnectionProvider::createShared({"httpbin.org", 80});
+  auto requestExecutor = oatpp::web::client::HttpRequestExecutor::createShared(connectionProvider);
+
+  oatpp::base::Environment::destroy();
   return 0;
 }
