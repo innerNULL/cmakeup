ea8f6eeb8a1f833565229d8f64debf336326f27f
innerNULL <lyt697@outlook.com>
2021-04-17 22:17:08 +0800

    [Feat] Initially adds oatpp-mbedtls cmake module script.

---
+++ cmakeup/pkg/oatpp/oatpp-mbedtls/demo/demo.cpp
@@ -0,0 +1,14 @@
+/// file: demo.cpp
+/// date: 2021-04-16
+
+
+#include <thread>
+#include "oatpp/network/tcp/client/ConnectionProvider.hpp"
+
+
+int main(int argc, char **argv) {
+  oatpp::base::Environment::init();
+  oatpp::base::Environment::destroy();
+  
+  return 0;
+}
