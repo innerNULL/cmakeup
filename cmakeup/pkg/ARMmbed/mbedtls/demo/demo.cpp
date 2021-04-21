d31c44c378efb1bc53ff7f07838b43e38cb155a0
innerNULL <lyt697@outlook.com>
2021-04-21 22:27:18 +0800

    [Fix] Fix cmakeup ';' bug.

--- cmakeup/pkg/ARMmbed/mbedtls/demo/demo.cpp
+++ cmakeup/pkg/ARMmbed/mbedtls/demo/demo.cpp
@@ -9,8 +9,13 @@
 #include "mbedtls/error.h"
 #include "mbedtls/entropy.h"
 #include "mbedtls/ctr_drbg.h"
+#include "mbedtls/platform.h"
 
 
 int main() {
+  const mbedtls_md_info_t *md_info;
+  mbedtls_md_context_t md_ctx;
+  mbedtls_md_init( &md_ctx );
+  mbedtls_md_free( &md_ctx );
   return 0;
 }
