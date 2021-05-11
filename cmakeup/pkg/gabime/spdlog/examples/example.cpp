cd36309c7aa2d3748ffd35ca6b054b2c2a5fd76e
innerNULL <lyt697@outlook.com>
2021-05-11 17:33:03 +0800

    [Feat] Supports gabime_spdlog pkg.

---
+++ cmakeup/pkg/gabime/spdlog/examples/example.cpp
@@ -0,0 +1,13 @@
+/// file: example.cpp
+/// date: 2021-05-11
+
+
+#include <iostream>
+
+#include <spdlog/spdlog.h>
+
+
+int main(int argc, char **argv) {
+  spdlog::info("Hello, World!");
+  return 0;
+}
